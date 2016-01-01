d713363f926160aca1c211e0e96eb08050486f35
Ambrosia <ambrosia@hana.re>
2016-01-01 17:12:49 +0000

    DiscordElixir.API.Gateway: Add documentation

--- lib/api/gateway.ex
+++ lib/api/gateway.ex
@@ -1,11 +1,27 @@
 defmodule DiscordElixir.API.Gateway do
   alias DiscordElixir.API
 
+  @doc """
+  Gets the WebSocket endpoint.
+
+  ## Example
+
+      iex> DiscordElixir.API.Gateway.endpoint
+      {:ok, "wss://gateway-fafnir.discord.gg"}
+  """
   def endpoint do
     with {:ok, response} <- API.get("/gateway"),
          do: {:ok, response.body["url"]}
   end
 
+  @doc """
+  Gets the WebSocket endpoint.
+
+  ## Example
+
+      iex> DiscordElixir.API.Gateway.endpoint1
+      "wss://gateway-fafnir.discord.gg"
+  """
   def endpoint! do
     API.get!("/gateway").body["url"]
   end
