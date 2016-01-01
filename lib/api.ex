1eaf3e172458033712d359cd8ac05e6d9560075a
Ambrosia <ambrosia@hana.re>
2016-01-01 14:40:16 +0000

    Add DiscordElixir.API
    This uses HTTPoison.Base, see
    http://hexdocs.pm/httpoison/HTTPoison.Base.html for function.
    
    Request and response bodies will be encoded and decoded by Poison. The
    token set in the config will be added to every request.

---
+++ lib/api.ex
@@ -0,0 +1,29 @@
+defmodule DiscordElixir.API do
+  use HTTPoison.Base
+
+  @url "https://discordapp.com/api"
+
+  defp process_url(url) do
+    @url <> url
+  end
+
+  defp process_request_headers(headers) do
+    add_token_header(headers)
+  end
+
+  defp process_request_body(map) do
+    Poison.encode!(map)
+  end
+
+  defp process_response_body(binary) do
+    Poison.decode!(binary)
+  end
+
+  defp add_token_header(headers) do
+    [{"Authorization", token} | headers]
+  end
+
+  defp token do
+    Application.get_env(:discord_elixir, :token)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule DiscordElixir.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,8 @@ defmodule DiscordElixir.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:poison, "~> 1.5"},
+     {:socket, "~> 0.3.1"},
+     {:httpoison, "~> 0.8.0"}]
   end
 end
