1a8c0bd9075dffc5e5f81679eddc8e75423e01bf
Ambrosia <ambrosia@hana.re>
2016-01-12 00:29:13 +0000

    Small code improvement in Keepalive handler

--- lib/discord_elixir/web_socket/handlers/keepalive.ex
+++ lib/discord_elixir/web_socket/handlers/keepalive.ex
@@ -21,30 +21,27 @@ defmodule DiscordElixir.WebSocket.Handlers.Keepalive do
   def handle_event({:message, msg = %{"t" => event_type}}, state)
   when event_type in @ready_states do
     Logger.debug("READY or RESUME event received")
-    {:ok, handle_heartbeat(msg, state, state.ws_client_pid)}
+    %{"d" => %{"heartbeat_interval" => interval}} = msg
+    {:ok, handle_heartbeat(interval, state, state.ws_client_pid)}
   end
 
   def handle_event(_, state) do
     {:ok, state}
   end
 
-  defp handle_heartbeat(%{"d" => %{"heartbeat_interval" => same}},
-                        state = %State{interval: same},
-                        _) do
+  defp handle_heartbeat(same, state = %State{interval: same}, _) do
     state
   end
 
-  defp handle_heartbeat(%{"d" => %{"heartbeat_interval" => new}},
-                        state = %State{interval: _old},
-                        ws_client_pid) do
+  defp handle_heartbeat(new_interval, state, ws_client_pid) do
     Logger.debug("New heartbeat interval detected")
-    {:ok, tref} = new_timer(new, ws_client_pid, state)
-    %State{state | interval: new, tref: tref}
+    {:ok, tref} = new_timer(new_interval, ws_client_pid, state)
+    %State{state | interval: new_interval, tref: tref}
   end
 
   defp new_timer(new_interval, ws_client_pid, state) do
-    :timer.cancel(state.tref)
     Logger.debug("New timer, sending keepalive every #{new_interval}ms")
+    :timer.cancel(state.tref)
     {:ok, tref} = :timer.apply_interval(new_interval,
                                         __MODULE__,
                                         :send_keepalive,
