284d708afe3fe80e9f2a1147b07e5d1a47572f4b
Ambrosia <ambrosia@hana.re>
2016-02-11 23:40:18 +0000

    Add example supervisor

---
+++ lib/datcord/examples/example_supervisor.ex
@@ -0,0 +1,30 @@
+defmodule Datcord.Examples.ExampleSupervisor do
+  use Supervisor
+  alias Datcord.WebSocket
+  alias Datcord.WebSocketHandlers.{Connect, Keepalive}
+
+  def start_link(token) do
+    result = {:ok, pid} = Supervisor.start_link(__MODULE__, [])
+    start_children(pid, token)
+    result
+  end
+
+  def init([]) do
+    supervise([], strategy: :rest_for_one)
+  end
+
+  def start_children(supervisor_pid, token) do
+    gen_event_spec = worker(GenEvent, [])
+    {:ok, gen_event} = Supervisor.start_child(supervisor_pid, gen_event_spec)
+
+    start_handlers(gen_event, token)
+
+    web_socket_spec = worker(WebSocket, [token, gen_event])
+    {:ok, _} = Supervisor.start_child(supervisor_pid, web_socket_spec)
+  end
+
+  defp start_handlers(gen_event, token) do
+    :ok = GenEvent.add_handler(gen_event, Connect, token)
+    :ok = GenEvent.add_handler(gen_event, Keepalive, [])
+  end
+end
